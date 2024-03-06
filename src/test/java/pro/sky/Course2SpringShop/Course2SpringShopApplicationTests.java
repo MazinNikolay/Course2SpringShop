8ab19cbf4df119d09c2b9ba1ad66162d4c69768a
Nikolay <koliamba_5@mail.ru>
2024-03-06 23:17:47 +0800

    Initial

---
+++ src/test/java/pro/sky/Course2SpringShop/Course2SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2SpringShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2SpringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
